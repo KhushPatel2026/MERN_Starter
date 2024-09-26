55e98b9ba7b1af37622f87606e7c898598e8fd57
KhushPatel2026 <khushspatel@gmail.com>
2024-09-26 10:24:33 +0530

    changes

--- Backend/Routes/profileRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { getProfile, editProfile } = require('../Controller/profileController')
-
-router.get('/profile', getProfile)
-router.post('/profile/edit', editProfile)
-
-module.exports = router
